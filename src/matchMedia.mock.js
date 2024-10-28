e2a31151173e1d7905c6ce6ba55846cb8c66b1dd
Sona <sona.gharibyann1@gmail.com>
2024-10-28 17:04:09 +0400

    minor

--- src/matchMedia.mock.js
+++ src/matchMedia.mock.js
@@ -3,8 +3,8 @@ window.matchMedia = (query) => ({
   matches: false,
   media: query,
   onchange: null,
-  addListener: jest.fn(), // deprecated//?
-  removeListener: jest.fn(), // deprecated
+  addListener: jest.fn(),
+  removeListener: jest.fn(), 
   addEventListener: jest.fn(),
   removeEventListener: jest.fn(),
   dispatchEvent: jest.fn(),
